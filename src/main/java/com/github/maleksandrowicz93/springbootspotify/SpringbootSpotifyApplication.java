aca8b0b42b390b0f4444048311f185ceeaa7d266
Mateusz Aleksandrowicz <mateusz.aleksandrowicz93@gmail.com>
2020-03-07 13:04:32 +0100

    initial commit

---
+++ src/main/java/com/github/maleksandrowicz93/springbootspotify/SpringbootSpotifyApplication.java
@@ -0,0 +1,13 @@
+package com.github.maleksandrowicz93.springbootspotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/maleksandrowicz93/springbootspotify/SpringbootSpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maleksandrowicz93.springbootspotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
